a0b21dda75348e2f2cd116224d27c7ac0f25aa4c
Vlad Bond <foolowin@gmail.com>
2021-01-27 16:30:35 +0300

    fixed ui

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,7 +6,12 @@ import testWebP from './components/webpService/webpService';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-
+    const links = document.querySelectorAll('a');
+    links.forEach((x) => {
+        x.addEventListener('click', (e) => {
+            e.preventDefault();
+        })
+    })
     // testWebP();
 
 
